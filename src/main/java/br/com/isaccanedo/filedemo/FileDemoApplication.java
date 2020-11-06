337a0d4b9528610de5a4a7be31694a8e54ee0b4b
isaccanedo <isaccanedo@gmail.com>
2020-11-06 14:43:29 -0300

    Initial commit

---
+++ src/main/java/br/com/isaccanedo/filedemo/FileDemoApplication.java
@@ -0,0 +1,17 @@
+package br.com.isaccanedo.filedemo;
+
+import br.com.isaccanedo.filedemo.property.FileStorageProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
+public class FileDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileDemoApplication.class, args);
+	}
+}
